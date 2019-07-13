f4197b780e6797bc4cd351a3055a456049948477
Alexandr <hodukovaleksandr@yandex.ru>
2019-07-13 16:48:37 +0300

    Add IncrementUtil class

---
+++ Server/src/main/java/com/chat/utils/IncrementUtil.java
@@ -0,0 +1,9 @@
+package com.chat.utils;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class IncrementUtil {
+	public static AtomicInteger incIdAgent;
+	public static AtomicInteger incIdClient;
+	public static AtomicInteger incIdChat;
+}
