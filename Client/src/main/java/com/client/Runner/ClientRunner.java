9fbeb528d3a6574fa0251f25b6a11d795d3ff468
Alexandr <hodukovaleksandr@yandex.ru>
2019-07-15 08:14:11 +0300

    Last fix

--- Client/src/main/java/com/client/Runner/ClientRunner.java
+++ Client/src/main/java/com/client/Runner/ClientRunner.java
@@ -3,13 +3,11 @@ package com.client.Runner;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 import com.client.Threads.InputThread;
 import com.client.Threads.OutputThread;
